c26226d24cc81e6221c803452c6f306ed6181f47
maxscherbakov <max-tsh@yandex.ru>
2023-12-15 16:21:48 +0300

    fix cmake

--- laboratory work/cmake_dir/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/work_area/finite-state-machine/laboratory work/cmake_dir/_deps/googletest-src/googletest
-# Build directory: /home/max/work_area/finite-state-machine/laboratory work/cmake_dir/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
